081b7632b392d323dd4243b10145eea0675d27fd
luiz felipe <lf92416@gmail.com>
2025-09-10 23:21:28 -0300

    feat: integrate Keycloak for authentication and add environment variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { KeycloakProvider } from '@react-keycloak/web'
+import keycloak from './keycloak'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const keycloakProviderInitConfig = {
+    onLoad: 'check-sso',
+  }
+
+  function onKeycloakEvent(event, error) {
+    console.log('onKeycloakEvent', event, error)
+  }
+
+  function onKeycloakTokens(tokens) {
+    console.log('onKeycloakTokens', tokens)
+  }
 
   return (
     <>
-      
+      <KeycloakProvider
+        keycloak={keycloak}
+        initConfig={keycloakProviderInitConfig}
+        onEvent={onKeycloakEvent}
+        onTokens={onKeycloakTokens}
+      >
+
+      </KeycloakProvider>
     </>
   )
 }
