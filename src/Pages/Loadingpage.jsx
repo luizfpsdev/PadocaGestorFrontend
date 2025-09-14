ac9ec5e0d0a67b7dd8f11a874c04d8713d32bf0c
luiz felipe <lf92416@gmail.com>
2025-09-14 00:12:41 -0300

    feat: enhance LoadingPage component with improved styling and structure

--- src/Pages/Loadingpage.jsx
+++ src/Pages/Loadingpage.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, VStack } from '@chakra-ui/react';
 import { ProgressCircle } from "@chakra-ui/react"
 
 const LoadingPage = () => (
     <Box bg="">
         <Flex height="100vh" alignItems="center" justifyContent="center" >
-            <ProgressCircle.Root value={null} size="lg">
-                <ProgressCircle.Circle css={{ "--thickness": "2px" }}>
-                    <ProgressCircle.Track />
-                    <ProgressCircle.Range />
-                </ProgressCircle.Circle>
-                <ProgressCircle.Label>Carregando...</ProgressCircle.Label>
+            <ProgressCircle.Root value={null} size="lg" colorPalette="orange" variant="soft">
+                <VStack>
+                    <ProgressCircle.Circle css={{ "--thickness": "2px" }}>
+                        <ProgressCircle.Track />
+                        <ProgressCircle.Range />
+                    </ProgressCircle.Circle>
+                    <ProgressCircle.Label color="orange.solid">Carregando...</ProgressCircle.Label>
+                </VStack>
             </ProgressCircle.Root>
         </Flex>
     </Box>
