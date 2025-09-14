0d5d11ba2848659d21d7288abbc46d2b438cfb0a
luiz felipe <lf92416@gmail.com>
2025-09-13 23:44:38 -0300

    feat: enhance HeaderComponent layout with additional spacers and restructured authentication display

--- src/components/Header/HeaderComponent.jsx
+++ src/components/Header/HeaderComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import style from './HeaderComponent.module.css';
 import { useAuth } from "react-oidc-context";
 import { ColorModeButton } from '../ui/color-mode';
-import { Tabs, Box, Avatar, Menu, Portal, HStack, Button, Separator, Flex } from "@chakra-ui/react";
+import { Tabs, Box, Avatar, Menu, Portal, HStack, Button, Separator, Flex, Spacer } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react"
 
 const HeaderComponent = () => {
@@ -11,35 +11,54 @@ const HeaderComponent = () => {
 
     return (
         <Box bg="Background" >
-            <Flex align="center" justify="space-around" gap={8}>
+            <Flex align="center" gap={8}>
                 <HStack wrap="wrap" gap="4">
                     <Image src="/padoca-logo.png" alt="Logo" boxSize="80px" objectFit="fill" />
+                    <Spacer />
+                    <Spacer />
+                    <Spacer />
+                    <Spacer />
+                    <Spacer />
+                    <Spacer />
+                    <Spacer />
+                    <Spacer />
+                    <Spacer />
+                    <Spacer />
                     <Separator orientation="vertical" height="4" />
                     <Button variant="ghost">Dashboard</Button>
                     <Button variant="ghost">Receitas</Button>
                     <Button variant="ghost">Produtos</Button>
                     <Button variant="ghost">Pessoal</Button>
                     <Separator orientation="vertical" height="4" />
-                   
-                        {auth.isAuthenticated && <span>Olá, {auth.user?.profile?.email}</span>}
-                        <div style={{ width: '10px' }}></div>
-                        {auth.isAuthenticated && (<Menu.Root positioning={{ placement: "right-start" }} colorPalette="orange" variant="subtle">
-                            <Menu.Trigger rounded="full" focusRing="outside">
-                                <Avatar.Root size="sm" variant="solid" colorPalette="orange">
-                                    <Avatar.Fallback name={auth.user?.profile?.name} />
-                                </Avatar.Root>
-                            </Menu.Trigger>
-                            <Portal>
-                                <Menu.Positioner >
-                                    <Menu.Content>
-                                        <Menu.Item value="profile">Profile</Menu.Item>
-                                        <Menu.Item value="logout" onClick={() => auth.signoutRedirect()}>Logout</Menu.Item>
-                                    </Menu.Content>
-                                </Menu.Positioner>
-                            </Portal>
-                        </Menu.Root>)}
-                    <Separator orientation="vertical" height="4" />
+                    <Spacer />
+                    <Spacer />
+                    <Spacer />
+                    <Spacer />
+                    <Spacer />
+                    <Spacer />
+                    <Spacer />
+                    <Spacer />
+                    <Spacer />
+                    <Spacer />
                     <ColorModeButton></ColorModeButton>
+                    <Separator orientation="vertical" height="4" />
+                    {auth.isAuthenticated && <span>Olá, {auth.user?.profile?.email}</span>}
+                    <div style={{ width: '10px' }}></div>
+                    {auth.isAuthenticated && (<Menu.Root positioning={{ placement: "right-start" }} colorPalette="orange" variant="subtle">
+                        <Menu.Trigger rounded="full" focusRing="outside">
+                            <Avatar.Root size="sm" variant="solid" colorPalette="orange">
+                                <Avatar.Fallback name={auth.user?.profile?.name} />
+                            </Avatar.Root>
+                        </Menu.Trigger>
+                        <Portal>
+                            <Menu.Positioner >
+                                <Menu.Content>
+                                    <Menu.Item value="profile">Profile</Menu.Item>
+                                    <Menu.Item value="logout" onClick={() => auth.signoutRedirect()}>Logout</Menu.Item>
+                                </Menu.Content>
+                            </Menu.Positioner>
+                        </Portal>
+                    </Menu.Root>)}
                 </HStack>
             </Flex>
         </Box>
