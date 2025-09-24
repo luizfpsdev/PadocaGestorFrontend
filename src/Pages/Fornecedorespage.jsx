474c98afa2e9b77d39ebb25ec1cf7a2dc7f3eb63
luiz felipe <lf92416@gmail.com>
2025-09-24 20:58:41 -0300

    feat: enhance FornecedoresPage with search functionality, selection checkboxes, and pagination improvements

--- src/Pages/Fornecedorespage.jsx
+++ src/Pages/Fornecedorespage.jsx
@@ -1,20 +1,29 @@
-import React, { use, useEffect } from 'react';
-import { Box, SimpleGrid, GridItem, Card, Stack, Field, Input, Badge, HStack, Button, Flex, Textarea,Table,Pagination,ButtonGroup,IconButton } from "@chakra-ui/react"
+import React, { useEffect, useState } from 'react';
+import { Box, SimpleGrid, GridItem, Group, Card, Stack, Field, Input, Badge, HStack, Button, Flex, Textarea, Table, Pagination, ButtonGroup, IconButton, Checkbox } from "@chakra-ui/react"
 import CardFuncionarioCadastroSkeleton from '../components/Skeletons/Cardfuncionariocadastroskeleton';
-import { LuChevronLeft, LuChevronRight } from "react-icons/lu"
+import { LuChevronLeft, LuChevronRight, LuSearch } from "react-icons/lu"
 
 
 const FornecedoresPage = () => {
 
     const [loading, setLoadingPage] = React.useState(false);
+    const [page, setPage] = useState(1);
+    const [selection, setSelection] = useState([])
 
     const items = [
-  { id: 1, nome: "Laptop", endereco: "Electronics", telefone: 999.99, cnpj: "12.345.678/0001-99" },
-  { id: 2, nome: "Coffee Maker", endereco: "Home Appliances", telefone: 49.99,cnpj: "12.345.678/0001-99" },
-  { id: 3, nome: "Desk Chair", endereco: "Furniture", telefone: 150.0,cnpj: "12.345.678/0001-99" },
-  { id: 4, nome: "Smartphone", endereco: "Electronics", telefone: 799.99,cnpj: "12.345.678/0001-99" },
-  { id: 5, nome: "Headphones", endereco: "Accessories", telefone: 199.99,cnpj: "12.345.678/0001-99" },
-]
+        { id: 1, nome: "Laptop", endereco: "Electronics", telefone: 999.99, cnpj: "12.345.678/0001-99" },
+        { id: 2, nome: "Coffee Maker", endereco: "Home Appliances", telefone: 49.99, cnpj: "12.345.678/0001-99" },
+        { id: 3, nome: "Desk Chair", endereco: "Furniture", telefone: 150.0, cnpj: "12.345.678/0001-99" },
+        { id: 4, nome: "Smartphone", endereco: "Electronics", telefone: 799.99, cnpj: "12.345.678/0001-99" },
+        { id: 5, nome: "Headphones", endereco: "Accessories", telefone: 199.99, cnpj: "12.345.678/0001-99" },
+         { id: 6, nome: "Smartphone", endereco: "Electronics", telefone: 799.99, cnpj: "12.345.678/0001-99" },
+        { id: 7, nome: "Headphones", endereco: "Accessories", telefone: 199.99, cnpj: "12.345.678/0001-99" },
+              { id: 8, nome: "Headphones", endereco: "Accessories", telefone: 199.99, cnpj: "12.345.678/0001-99" },
+         { id: 9, nome: "Smartphone", endereco: "Electronics", telefone: 799.99, cnpj: "12.345.678/0001-99" },
+        { id: 10, nome: "Headphones", endereco: "Accessories", telefone: 199.99, cnpj: "12.345.678/0001-99" },
+    ]
+
+    const indeterminate = selection.length > 0 && selection.length < items.length
 
     useEffect(() => {
         setTimeout(() => {
@@ -75,13 +84,35 @@ const FornecedoresPage = () => {
                     <GridItem colSpan={7}>
                         <Card.Root>
                             <Card.Header>
-
+                                <Group attached w="full" maxW="sm">
+                                    <Input flex="1" placeholder="Entre com o termo pesquisado" />
+                                    <IconButton colorPalette="orange" variant="solid">
+                                        <LuSearch />
+                                    </IconButton>
+                                </Group>
                             </Card.Header>
                             <Card.Body>
-                                <Table.Root size="lg" variant="line"  interactive>
+                                <Table.Root size="lg" variant="line" interactive colorPalette="orange" striped>
                                     <Table.Header>
-                                        <Table.Row>
-                                            <Table.ColumnHeader>#</Table.ColumnHeader>
+                                        <Table.Row >
+                                            <Table.ColumnHeader>
+                                                <Checkbox.Root
+                                                    size="sm"
+                                                    mt="0.5"
+                                                    aria-label="Select all rows"
+                                                    checked={indeterminate ? "indeterminate" : selection.length > 0}
+                                                    onCheckedChange={(changes) => {
+                                                        setSelection(
+                                                            changes.checked ? items.map((item) => item.id) : [],
+                                                        )
+                                                    }}
+                                                >
+                                                    <Checkbox.HiddenInput />
+                                                    <Checkbox.Control />
+                                                </Checkbox.Root>
+                                            </Table.ColumnHeader>
+                                            <Table.ColumnHeader>#
+                                            </Table.ColumnHeader>
                                             <Table.ColumnHeader>Nome</Table.ColumnHeader>
                                             <Table.ColumnHeader>Endereço</Table.ColumnHeader>
                                             <Table.ColumnHeader>Telefone</Table.ColumnHeader>
@@ -91,10 +122,28 @@ const FornecedoresPage = () => {
                                     </Table.Header>
                                     <Table.Body>
                                         {items.map((item) => (
-                                            <Table.Row key={item.id}>
+                                            <Table.Row key={item.id} data-selected={selection.includes(item.id) ? "" : undefined}>
+                                                <Table.Cell>
+                                                    <Checkbox.Root
+                                                        size="sm"
+                                                        mt="0.5"
+                                                        aria-label="Select row"
+                                                        checked={selection.includes(item.id)}
+                                                        onCheckedChange={(changes) => {
+                                                            setSelection((prev) =>
+                                                                changes.checked
+                                                                    ? [...prev, item.id]
+                                                                    : selection.filter((id) => id !== item.id),
+                                                            )
+                                                        }}
+                                                    >
+                                                        <Checkbox.HiddenInput />
+                                                        <Checkbox.Control />
+                                                    </Checkbox.Root>
+                                                </Table.Cell>
                                                 <Table.Cell>{item.id}</Table.Cell>
                                                 <Table.Cell>{item.nome}</Table.Cell>
-                                                 <Table.Cell>{item.endereco}</Table.Cell>
+                                                <Table.Cell>{item.endereco}</Table.Cell>
                                                 <Table.Cell>{item.telefone}</Table.Cell>
                                                 <Table.Cell>{item.cnpj}</Table.Cell>
                                                 <Table.Cell textAlign="end">{item.price}</Table.Cell>
@@ -103,33 +152,38 @@ const FornecedoresPage = () => {
                                     </Table.Body>
                                 </Table.Root>
 
-                               
+
                             </Card.Body>
-                            <Card.Footer>
-                                 <Pagination.Root count={items.length * 5} pageSize={5} page={1}>
-                                    <ButtonGroup variant="ghost" size="sm" wrap="wrap">
-                                        <Pagination.PrevTrigger asChild>
-                                            <IconButton>
-                                                <LuChevronLeft />
-                                            </IconButton>
-                                        </Pagination.PrevTrigger>
+                            <Flex justifyContent={'space-around'} >
+                                <Card.Footer>
 
-                                        <Pagination.Items
-                                            render={(page) => (
-                                                <IconButton variant={{ base: "ghost", _selected: "outline" }}>
-                                                    {page.value}
+                                    <Pagination.Root count={items.length * 1000} pageSize={5} page={page} onPageChange={(e) => setPage(e.page)} colorPalette="orange">
+
+                                        <ButtonGroup variant="outline" size="lg" wrap="wrap" attached >
+                                            <Pagination.PrevTrigger asChild>
+                                                <IconButton>
+                                                    <LuChevronLeft />
                                                 </IconButton>
-                                            )}
-                                        />
+                                            </Pagination.PrevTrigger>
 
-                                        <Pagination.NextTrigger asChild>
-                                            <IconButton>
-                                                <LuChevronRight />
-                                            </IconButton>
-                                        </Pagination.NextTrigger>
-                                    </ButtonGroup>
-                                </Pagination.Root>
-                            </Card.Footer>
+                                            <Pagination.Items
+                                                render={(page) => (
+                                                    <IconButton variant={{ base: "outline", _selected: "solid" }}>
+                                                        {page.value}
+                                                    </IconButton>
+                                                )}
+                                            />
+
+                                            <Pagination.NextTrigger asChild>
+                                                <IconButton>
+                                                    <LuChevronRight />
+                                                </IconButton>
+                                            </Pagination.NextTrigger>
+                                        </ButtonGroup>
+                                    </Pagination.Root>
+                                </Card.Footer>
+                            </Flex>
+
                         </Card.Root>
                     </GridItem>
                 </SimpleGrid>
