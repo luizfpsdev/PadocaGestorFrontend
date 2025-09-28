c7939c6e7e730656a461f2b4fb2a78608f5189b0
luiz felipe <lf92416@gmail.com>
2025-09-28 01:51:58 -0300

    feat: add confirmation dialog for item deletion in FornecedoresPage

--- src/Pages/Fornecedorespage.jsx
+++ src/Pages/Fornecedorespage.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Box, SimpleGrid, GridItem, Group, Card, Stack, Field, Input, Badge, HStack, Button, Flex, Textarea, Table, Pagination, ButtonGroup, IconButton, Checkbox } from "@chakra-ui/react"
+import { Box, SimpleGrid, GridItem, Group, Card, Stack, Field, Input, Text, CloseButton, Badge, Dialog, Portal, HStack, Button, Flex, Textarea, Table, Pagination, ButtonGroup, IconButton, Checkbox } from "@chakra-ui/react"
 import CardFuncionarioCadastroSkeleton from '../components/Skeletons/Cardfuncionariocadastroskeleton';
-import { LuChevronLeft, LuChevronRight, LuSearch,LuSquarePen,LuTrash2 } from "react-icons/lu"
+import { LuChevronLeft, LuChevronRight, LuSearch, LuSquarePen, LuTrash2 } from "react-icons/lu"
 
 
 const FornecedoresPage = () => {
 
     const [loading, setLoadingPage] = React.useState(false);
     const [page, setPage] = useState(1);
+    const [open, setOpen] = useState(false);
+    const [idItem, setIdItem] = useState(null);
     const [selection, setSelection] = useState([])
 
     const items = [
@@ -16,10 +18,10 @@ const FornecedoresPage = () => {
         { id: 3, nome: "Desk Chair", endereco: "Furniture", telefone: 150.0, cnpj: "12.345.678/0001-99" },
         { id: 4, nome: "Smartphone", endereco: "Electronics", telefone: 799.99, cnpj: "12.345.678/0001-99" },
         { id: 5, nome: "Headphones", endereco: "Accessories", telefone: 199.99, cnpj: "12.345.678/0001-99" },
-         { id: 6, nome: "Smartphone", endereco: "Electronics", telefone: 799.99, cnpj: "12.345.678/0001-99" },
+        { id: 6, nome: "Smartphone", endereco: "Electronics", telefone: 799.99, cnpj: "12.345.678/0001-99" },
         { id: 7, nome: "Headphones", endereco: "Accessories", telefone: 199.99, cnpj: "12.345.678/0001-99" },
-              { id: 8, nome: "Headphones", endereco: "Accessories", telefone: 199.99, cnpj: "12.345.678/0001-99" },
-         { id: 9, nome: "Smartphone", endereco: "Electronics", telefone: 799.99, cnpj: "12.345.678/0001-99" },
+        { id: 8, nome: "Headphones", endereco: "Accessories", telefone: 199.99, cnpj: "12.345.678/0001-99" },
+        { id: 9, nome: "Smartphone", endereco: "Electronics", telefone: 799.99, cnpj: "12.345.678/0001-99" },
         { id: 10, nome: "Headphones", endereco: "Accessories", telefone: 199.99, cnpj: "12.345.678/0001-99" },
     ]
 
@@ -93,89 +95,83 @@ const FornecedoresPage = () => {
                             </Card.Header>
                             <Card.Body>
                                 <Table.ScrollArea height="500px">
- <Table.Root size="lg" variant="line" interactive colorPalette="orange" stickyHeader>
-                                    <Table.Header >
-                                        <Table.Row >
-                                            <Table.ColumnHeader>
-                                                <Checkbox.Root
-                                                    size="sm"
-                                                    mt="0.5"
-                                                    aria-label="Select all rows"
-                                                    checked={indeterminate ? "indeterminate" : selection.length > 0}
-                                                    onCheckedChange={(changes) => {
-                                                        setSelection(
-                                                            changes.checked ? items.map((item) => item.id) : [],
-                                                        )
-                                                    }}
-                                                >
-                                                    <Checkbox.HiddenInput />
-                                                    <Checkbox.Control />
-                                                </Checkbox.Root>
-                                            </Table.ColumnHeader>
-                                            <Table.ColumnHeader>#
-                                            </Table.ColumnHeader>
-                                            <Table.ColumnHeader>Nome</Table.ColumnHeader>
-                                            <Table.ColumnHeader>Endereço</Table.ColumnHeader>
-                                            <Table.ColumnHeader>Telefone</Table.ColumnHeader>
-                                            <Table.ColumnHeader>Cnpj</Table.ColumnHeader>
-                                            <Table.ColumnHeader >Ações</Table.ColumnHeader>
-                                        </Table.Row>
-                                    </Table.Header>
-                                    <Table.Body>
-                                        {items.map((item) => (
-                                            <Table.Row key={item.id} data-selected={selection.includes(item.id) ? "" : undefined}>
-                                                <Table.Cell>
+                                    <Table.Root size="lg" variant="line" interactive colorPalette="orange" stickyHeader>
+                                        <Table.Header >
+                                            <Table.Row >
+                                                <Table.ColumnHeader>
                                                     <Checkbox.Root
                                                         size="sm"
                                                         mt="0.5"
-                                                        aria-label="Select row"
-                                                        checked={selection.includes(item.id)}
+                                                        aria-label="Select all rows"
+                                                        checked={indeterminate ? "indeterminate" : selection.length > 0}
                                                         onCheckedChange={(changes) => {
-                                                            setSelection((prev) =>
-                                                                changes.checked
-                                                                    ? [...prev, item.id]
-                                                                    : selection.filter((id) => id !== item.id),
+                                                            setSelection(
+                                                                changes.checked ? items.map((item) => item.id) : [],
                                                             )
                                                         }}
                                                     >
                                                         <Checkbox.HiddenInput />
                                                         <Checkbox.Control />
                                                     </Checkbox.Root>
-                                                </Table.Cell>
-                                                <Table.Cell>{item.id}</Table.Cell>
-                                                <Table.Cell>{item.nome}</Table.Cell>
-                                                <Table.Cell>{item.endereco}</Table.Cell>
-                                                <Table.Cell>{item.telefone}</Table.Cell>
-                                                <Table.Cell>{item.cnpj}</Table.Cell>
-                                                <Table.Cell >
-                                                    <HStack>
-                                                        <IconButton size="sm" colorPalette="blue" variant={'subtle'}><LuSquarePen></LuSquarePen></IconButton>
-                                                        <IconButton size="sm" colorPalette="red" variant="solid">
-                                                            <LuTrash2></LuTrash2>
-                                                        </IconButton>
-                                                    </HStack>
-                                                </Table.Cell>
+                                                </Table.ColumnHeader>
+                                                <Table.ColumnHeader>#
+                                                </Table.ColumnHeader>
+                                                <Table.ColumnHeader>Nome</Table.ColumnHeader>
+                                                <Table.ColumnHeader>Endereço</Table.ColumnHeader>
+                                                <Table.ColumnHeader>Telefone</Table.ColumnHeader>
+                                                <Table.ColumnHeader>Cnpj</Table.ColumnHeader>
+                                                <Table.ColumnHeader >Ações</Table.ColumnHeader>
                                             </Table.Row>
-                                        ))}
-                                    </Table.Body>
-                                </Table.Root>
+                                        </Table.Header>
+                                        <Table.Body>
+                                            {items.map((item) => (
+                                                <Table.Row key={item.id} data-selected={selection.includes(item.id) ? "" : undefined}>
+                                                    <Table.Cell>
+                                                        <Checkbox.Root
+                                                            size="sm"
+                                                            mt="0.5"
+                                                            aria-label="Select row"
+                                                            checked={selection.includes(item.id)}
+                                                            onCheckedChange={(changes) => {
+                                                                setSelection((prev) =>
+                                                                    changes.checked
+                                                                        ? [...prev, item.id]
+                                                                        : selection.filter((id) => id !== item.id),
+                                                                )
+                                                            }}
+                                                        >
+                                                            <Checkbox.HiddenInput />
+                                                            <Checkbox.Control />
+                                                        </Checkbox.Root>
+                                                    </Table.Cell>
+                                                    <Table.Cell>{item.id}</Table.Cell>
+                                                    <Table.Cell>{item.nome}</Table.Cell>
+                                                    <Table.Cell>{item.endereco}</Table.Cell>
+                                                    <Table.Cell>{item.telefone}</Table.Cell>
+                                                    <Table.Cell>{item.cnpj}</Table.Cell>
+                                                    <Table.Cell >
+                                                        <HStack>
+                                                            <IconButton size="sm" colorPalette="blue" variant={'subtle'}><LuSquarePen></LuSquarePen></IconButton>
+                                                            <IconButton size="sm" colorPalette="red" variant="solid" onClick={() => {setOpen(true) ; setIdItem(item.id)}}>
+                                                                <LuTrash2></LuTrash2>
+                                                            </IconButton>
+                                                        </HStack>
+                                                    </Table.Cell>
+                                                </Table.Row>
+                                            ))}
+                                        </Table.Body>
+                                    </Table.Root>
                                 </Table.ScrollArea>
-                               
-
-
                             </Card.Body>
                             <Flex justifyContent={'space-around'} >
                                 <Card.Footer>
-
                                     <Pagination.Root count={items.length * 1000} pageSize={5} page={page} onPageChange={(e) => setPage(e.page)} colorPalette="orange">
-
                                         <ButtonGroup variant="outline" size="lg" wrap="wrap" attached >
                                             <Pagination.PrevTrigger asChild>
                                                 <IconButton>
                                                     <LuChevronLeft />
                                                 </IconButton>
                                             </Pagination.PrevTrigger>
-
                                             <Pagination.Items
                                                 render={(page) => (
                                                     <IconButton variant={{ base: "outline", _selected: "solid" }}>
@@ -183,7 +179,6 @@ const FornecedoresPage = () => {
                                                     </IconButton>
                                                 )}
                                             />
-
                                             <Pagination.NextTrigger asChild>
                                                 <IconButton>
                                                     <LuChevronRight />
@@ -193,12 +188,32 @@ const FornecedoresPage = () => {
                                     </Pagination.Root>
                                 </Card.Footer>
                             </Flex>
-
                         </Card.Root>
                     </GridItem>
                 </SimpleGrid>
             </Box>)}
 
+            <Dialog.Root open={open} onOpenChange={(e) => setOpen(e.open)} role="alertdialog">
+                <Portal>
+                    <Dialog.Backdrop />
+                    <Dialog.Positioner>
+                        <Dialog.Content>
+                            <Dialog.Body>
+                                <Text>Tem certeza que deseja deletar o item cujo id é {idItem}?</Text>
+                            </Dialog.Body>
+                            <Dialog.Footer>
+                                <Dialog.ActionTrigger asChild>
+                                    <Button variant="outline">Cancelar</Button>
+                                </Dialog.ActionTrigger>
+                                <Button colorPalette="red">Confirmar</Button>
+                            </Dialog.Footer>
+                            <Dialog.CloseTrigger asChild>
+                                <CloseButton size="sm" />
+                            </Dialog.CloseTrigger>
+                        </Dialog.Content>
+                    </Dialog.Positioner>
+                </Portal>
+            </Dialog.Root>
         </>
     );
 };
