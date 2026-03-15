9fcaad5f31cad8c8919eb8b27011dea79a3c1841
luiz felipe <lf92416@gmail.com>
2026-03-14 22:31:15 -0300

    feat: Add ReceitasPage and DashboardPage components; update routing in App component; remove old DashboardPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import DashboardPage from "./components/Dashboardpage";
 import CallbackPage from "./Pages/Callbackpage";
 import { useAuth } from "react-oidc-context";
 import ContainerPrincipal from './components/ContainerPrincipal';
@@ -8,6 +7,9 @@ import LoadingPage from './Pages/Loadingpage';
 import FornecedoresPage from './Pages/Fornecedorespage';
 import ProductsPage from './Pages/Productspage';
 import IngredientesPage from './Pages/Ingredientespage';
+import ReceitasPage from './Pages/Receitaspage';
+import ComparativoPage from './Pages/Comparativopage';
+import DashboardPage from './Pages/Dashboardpage';
 
 function App() {
 
@@ -22,11 +24,11 @@ function App() {
           <Route element={<ContainerPrincipal />}>
             <Route path="/" element={<Navigate to="/dashboard" />} />
             <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/receitas" element={<LoadingPage />} />
+            <Route path="/receitas" element={<ReceitasPage />} />
             <Route path="/produtos" element={<ProductsPage/>} />
             <Route path="/fornecedores" element={<FornecedoresPage/>} />
             <Route path="/custos" element={<IngredientesPage />} />
-            <Route path="/comparativo" element={<LoadingPage />} />
+            <Route path="/comparativo" element={<ComparativoPage />} />
             <Route path="/lucro" element={<LoadingPage />} />
             <Route path="/callback" element={<CallbackPage />} />
           </Route>
